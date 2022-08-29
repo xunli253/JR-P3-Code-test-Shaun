aaa2dd5ae718bb6dbe118e267b44b5620584bb5f
Xun Li <xunli253@gmail.com>
2022-08-29 16:31:41 +1000

    initial update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 // write your code here
+const printer = () => {
+    for (let i = 0; i < 101; i++) {
+
+        i % 3 === 0 && i % 5 !== 0 && console.log("Fizz")
+        i % 5 === 0 && i % 3 !== 0 && console.log("Buzz")
+        i % 5 === 0 && i % 3 === 0 && console.log("FizzBuzz")
+        i % 5 !== 0 && i % 3 !== 0 && console.log(i)
 
 
+    }
+}
+
+
+
+
+printer();
